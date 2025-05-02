8929351b94728505b1984f6ce27368b624eb413e
shahadat <nayemshahadat581@gmail.com>
2025-05-02 12:46:17 +0600

    ticket details doing

--- src/app/(commonlayout)/Bus/[id]/page.tsx
+++ src/app/(commonlayout)/Bus/[id]/page.tsx
@@ -1,7 +1,11 @@
-const BookTicket=()=>{
-    return(
-        <div className="p-5">
-        zdfgdsg
+
+import TicketDetails from "@/components/Ticket/TicketDetails"
+const BookTicket = () => {
+    return (
+        <div className="mx-auto mt-8 min-h-[80vh] p-5 md:p-8
+         bg-gray-100/50 rounded-3xl border-t-[3px] w-[94%] border-green-500">
+           <TicketDetails></TicketDetails>
+           
         </div>
     )
 }
---
+++ src/components/Ticket/TicketDetails.tsx
@@ -0,0 +1,55 @@
+import { Bus } from "lucide-react"
+import Image from "next/image"
+import driver from '../../assets/info-devider.png'
+const TicketDetails = () => {
+    return (
+        <div className="flex flex-col md:flex-row items-center gap-4 md:gap-8 justify-between min-h-[300px] max-w-[1200px] mx-auto bg-white rounded-2xl">
+            {/* 1st div */}
+            <div className="flex-grow w-full md:w-auto p-5">
+                <div className="flex justify-between items-center">
+                    <div className="flex space-x-2 items-center">
+                        <div>
+                            <Bus className="size-6 md:size-8"></Bus>
+                        </div>
+                        <div>
+                            <h1 className="font-semibold text-[16px] md:text-xl">Hanif Enterprise</h1>
+                            <p className="text-gray-400 text-sm">HN-86908C</p>
+                        </div>
+                    </div>
+                    <div className="flex px-1 md:px-2 space-x-0.5 h-[26px] items-center bg-green-100 rounded-full">
+                        <Bus className="size-4 text-green-500"></Bus>
+                        <p className="text-green-500 text-sm">Seat:40</p>
+                    </div>
+                </div>
+                <div className="mt-2 p-5 bg-gray-100 rounded-2xl">
+                    <div className="flex justify-between py-4 border-b-3 border-dashed">
+                        <h1 className="text-sm text-gray-500">Departure Time</h1>
+                        <h1 className="text-sm text-gray-500">9:00PM</h1>
+                    </div>
+                    <div className="flex justify-between py-4">
+                        <h1 className="text-sm text-gray-500">Syllhet</h1>
+                        <h1 className="text-sm text-gray-500">To</h1>
+                        <h1 className="text-sm text-gray-500">Chittagong</h1>
+
+                    </div>
+                </div>
+            </div>
+            {/* 2nd div */}
+            <div className="hidden md:flex">
+                <Image src={driver} width={58} height={380} alt="image"></Image>
+            </div>
+            {/* 3rd div */}
+            <div className="w-[120px] pb-5 flex gap-4 flex-row md:flex-col">
+
+                <h1 className="flex text-xl font-semibold items-center justify-center
+             h-[40px] w-[40px] rounded-full bg-green-300">৳</h1>
+                <div>
+                    <h1 className="text-xl font-semibold">550</h1>
+                    <p className="font-semibold text-sm">Per Seat</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default TicketDetails
