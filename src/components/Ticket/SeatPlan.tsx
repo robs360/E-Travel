1ee8edac5c8774a73dfd8a7a0f1d0a9346bd0425
shahadat <nayemshahadat581@gmail.com>
2025-05-03 12:06:47 +0600

    ticket slection done

--- src/components/Ticket/SeatPlan.tsx
+++ src/components/Ticket/SeatPlan.tsx
@@ -2,16 +2,25 @@
 import Image from 'next/image'
 import stearing from '../../assets/steering-wheel.png'
 import { useState } from 'react'
+import toast from 'react-hot-toast'
+import { Button } from '../ui/button'
 const SeatPlan = () => {
     const [bookedTicket, setBookedTicket] = useState<string[]>([]);
+    const [counter, setCounter] = useState(0)
 
-    console.log(bookedTicket)
     const handleTicket = (seat: string) => {
-        console.log(seat)
+        const count = counter + 1;
+
+        if (count > 4) {
+            toast.error("You can book maximum 4 tickets");
+            return;
+        }
+
+        setCounter(count);
         setBookedTicket(prev => [...prev, seat]);
-    }
+    };
     return (
-        <div className="flex gap-6  mt-8 min-h-[300px] max-w-[1200px] p-5 mx-auto bg-white rounded-2xl">
+        <div className="flex gap-4  mt-8 min-h-[300px] max-w-[1200px] p-6 mx-auto bg-white rounded-2xl justify-between">
             <div className='max-w-[500px]'>
                 <div className='w-[60px] ml-auto'>
                     <Image src={stearing} alt='' width={50}></Image>
@@ -26,8 +35,8 @@ const SeatPlan = () => {
                             return (
                                 <button onClick={() => { handleTicket(`${row}${seatNum}`) }}
                                     key={`L-${row}${seatNum}`}
-                                    className={`${bookedTicket.includes(seat) ? 'bg-green-400' : 'bg-gray-100/50'
-                                        } text-black cursor-pointer font-semibold py-2 rounded shadow w-16`}
+                                    className={`${bookedTicket.includes(seat) ? 'bg-green-500 text-white' : 'bg-gray-100/50'
+                                        } text-black cursor-pointer font-semibold py-2 rounded shadow w-14`}
                                 >
                                     {row}{seatNum}
                                 </button>
@@ -44,7 +53,7 @@ const SeatPlan = () => {
                             return (
                                 <button onClick={() => { handleTicket(`${row}${seatNum}`) }}
                                     key={`R-${row}${seatNum}`}
-                                    className={`${bookedTicket.includes(seat)?'bg-green-400':'bg-gray-100/50'} text-black cursor-pointer font-semibold py-2 rounded shadow w-16`}
+                                    className={`${bookedTicket.includes(seat) ? 'bg-green-500 text-white' : 'bg-gray-100/50'} text-black cursor-pointer font-semibold py-2 rounded shadow w-14`}
                                 >
                                     {row}{seatNum}
                                 </button>
@@ -55,8 +64,22 @@ const SeatPlan = () => {
 
 
             </div>
-            <div>
+            <div className='pl-5 md:pl-9 lg:pl-12 border-dotted border-l-2'>
+                <h1 className='bg-red-200 text-red-500 animate-pulse
+                 px-1.5 py-0.5 rounded-full mb-2'>Maximum 4 seat can bok</h1>
                 <h1 className='font-semibold'>Selected Seat</h1>
+                <div className='mt-2 flex px-2.5 py-1.5 justify-between bg-gray-100/50'>
+                    {
+                        bookedTicket?.map((_, index: number) =>
+                            <span key={index}>{bookedTicket[index]}</span>)
+                    }
+                </div>
+                {
+                    bookedTicket.length > 0 && (<div>
+                        <Button className='mt-2 font-semibold bg-green-500
+                         hover:bg-green-600 block mx-auto'>Confirm Ticket</Button>
+                    </div>)
+                }
             </div>
 
         </div>
