3b3e52cd27f62a685f1994a1fbb84f6646253cd1
shahadat <nayemshahadat581@gmail.com>
2025-05-25 10:25:39 +0600

    login user adding

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,7 +1,6 @@
 'use server'
-
 import { Tuser } from "@/types/types"
-
+  
 export const RegisterUser = async (user: Tuser) => {
     try {
         const res = await fetch(`${process.env.api_end_point}api/v1/user/register`, {
@@ -10,6 +9,24 @@ export const RegisterUser = async (user: Tuser) => {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(user)
+        })
+        const ressult = await res.json()
+        return ressult
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+export const loginUser = async (userData: { emai: string, password: string }) => {
+    try {
+
+        const res = await fetch(`${process.env.api_end_point}api/v1/user/login`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData)
         })
         const ressult = await res.json()
         return ressult
